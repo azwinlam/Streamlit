5701c11ca3aaf629ea6ac19e84371480d7cf5baf
azwinlam <azwin.lam@gmail.com>
2021-03-17 18:10:39 +0800

    .

--- oprk/opk.py
+++ oprk/opk.py
@@ -1,14 +1,24 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+    page_title="One Place Foodie",
+    )
+
 @st.cache
 def default_csv():
-    return pd.read_csv("temp_df.csv", header=0)
+    return pd.read_csv("temp_df.csv", index_col=0, header=0)
 
 st.title('Welcome to OPK')
 st.header("By Trio")
 st.header("")
 
 df = default_csv()
+
+pick_district = st.sidebar.selectbox(
+        'Pick Location:',
+        df['district_en'].unique())
+
+df = df[df.district_en == pick_district]
 
 st.write(df)
