8457362e6f81b15031f961de6e4e6068144add84
azwinlam <azwin.lam@gmail.com>
2021-03-18 10:08:59 +0800

    v4

--- oprk/opk.py
+++ oprk/opk.py
@@ -24,7 +24,7 @@ pick_district = st.sidebar.selectbox(
 
 pick_cuisine = st.sidebar.selectbox(
         'Pick Cuisine:',
-        df['cuisine'].unique())
+        df['cuisine'].unique().sort())
 
 df = df.loc[(df['district_en'] == pick_district) & (df['cuisine'] == pick_cuisine)]
 
