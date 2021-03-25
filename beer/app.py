96db2657bdee9adbf25cd6d4778b90611370f8a0
azwinlam <azwin.lam@gmail.com>
2021-03-25 22:06:03 +0800

    .

--- beer/app.py
+++ beer/app.py
@@ -25,6 +25,38 @@ st.set_page_config(
 def load_csv():
     return pd.read_csv("df_price.csv",header=0,index_col=0)
 
+@st.cache(suppress_st_warning=True)
+def load_model():
+    original_image = Image.open(uploaded_file)
+    
+    fixed_image = ImageOps.exif_transpose(original_image)
+    image_to_resize = img_to_array(fixed_image)
+      
+    resized = tf.image.resize(image_to_resize, [224, 168], method="bilinear",antialias=False)
+    img_array = tf.keras.preprocessing.image.img_to_array(resized)
+      
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+      
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+      
+    img_show = tf.squeeze(img_array , axis=None, name=None)
+    
+    
+    predicted_class = class_names[np.argmax(score)]
+      
+    st.write(f"This image most likely belongs to {predicted_class}")
+      
+    percentages = [i * 100 for i in predictions.tolist()[0]]
+      
+    results = zip(class_names, percentages)
+    sorted_by_second = sorted(results, key=lambda tup: tup[1],reverse=True)
+    for i in sorted_by_second[:2]:
+        st.write(i)
+    
+    return predicted_class
+      
+
 
 st.title("Beer Price Check V3")
 st.subheader("By Alex, Azwin, Jason")
@@ -55,7 +87,7 @@ if uploaded_file is not None:
           fixed_image = ImageOps.exif_transpose(original_image)
           image_to_resize = img_to_array(fixed_image)
         
-          resized = tf.image.resize(image_to_resize, [224, 224], method="bilinear",antialias=False)
+          resized = tf.image.resize(image_to_resize, [224, 168], method="bilinear",antialias=False)
           img_array = tf.keras.preprocessing.image.img_to_array(resized)
         
           img_array = tf.expand_dims(img_array, 0) # Create a batch
@@ -83,89 +115,22 @@ if uploaded_file is not None:
           pass
     else:
         try:
+          predicted_class = load_model()
+          col1.image(Image.open(uploaded_file))
           original_image = Image.open(uploaded_file)
-          col1.image(original_image)
-          fixed_image = ImageOps.exif_transpose(original_image)
-          image_to_resize = img_to_array(fixed_image)
-        
-          resized = tf.image.resize(image_to_resize, [224, 168], method="bilinear",antialias=False)
-          img_array = tf.keras.preprocessing.image.img_to_array(resized)
-        
-          img_array = tf.expand_dims(img_array, 0) # Create a batch
-        
-          predictions = model.predict(img_array)
-          score = tf.nn.softmax(predictions[0])
-        
-          img_show = tf.squeeze(
-              img_array , axis=None, name=None)
-          
-          
-          predicted_class = class_names[np.argmax(score)]
-        
-          st.write(f"This image most likely belongs to {predicted_class}")
-        
-          percentages = [i * 100 for i in predictions.tolist()[0]]
-        
-          results = zip(class_names, percentages)
-          sorted_by_second = sorted(results, key=lambda tup: tup[1],reverse=True)
-          for i in sorted_by_second[:2]:
-            st.write(i)
                    
         except:
           pass
     
-    # #Grab Test Pictures
-    # test_dir = pathlib.Path("./pictures/")
-    # TEST = list(test_dir.glob("*"))
-    # len(TEST)
-    
-    # for string in TEST: 
-    #       # print(string)
-    #       try:
-    #         original_image = Image.open(str(string))
-    #         fixed_image = ImageOps.exif_transpose(original_image)
-    #         image_to_resize = img_to_array(fixed_image)
-    
-    #         resized = tf.image.resize(image_to_resize, [224, 224], method="bilinear",antialias=False)
-    #         img_array = tf.keras.preprocessing.image.img_to_array(resized)
-    
-    #         img_array = tf.expand_dims(img_array, 0) # Create a batch
-    
-    #         predictions = model.predict(img_array)
-    #         score = tf.nn.softmax(predictions[0])
-    
-    #         img_show = tf.squeeze(
-    #             img_array , axis=None, name=None)
-    
-    
-    #         print(
-    #             "This image most likely belongs to {} with a {:.2f} percent confidence."
-    #             .format(class_names[np.argmax(score)], 100 * np.max(score))
-    #         )
-    
-    #         percentages = [i * 100 for i in predictions.tolist()[0]]
-    
-    #         results = zip(class_names, percentages)
-    #         sorted_by_second = sorted(results, key=lambda tup: tup[1],reverse=True)
-    
-    
-    #         for i in sorted_by_second[:2]:
-    #           print(i)
-    
-    #         plt.imshow(resized/255.);
-    #         plt.axis(False);
-    #         plt.show()
-    #         print("----------------------------------------------")
-    #         print(" ")
-    
-    #       except:
-    #         pass
-        
-    # ##------------------------------------------------------------------------    
     df = load_csv()
-    # df = df.fillna("--")
+    # df = df.fillna("--") 
     st.header("Best Prices Found")
-    temp_df = df[df.Brand==predicted_class.title()]
+    
+    @st.cache
+    def temp_df():
+        return df[df.Brand==predicted_class.title()]
+    
+    temp_df = temp_df()
     
     
    
@@ -179,12 +144,12 @@ if uploaded_file is not None:
         original_image = original_image.save(f"./pictures/{correct}_{predicted_class}_{timestr}.jpg")
 
     if col2.button("No"):
-        col2.text("Please take another photo")
+        col2.text("Please take a photo with focus on the logo")
         correct = "False"
         original_image = original_image.save(f"./pictures/{correct}_{predicted_class}_{timestr}.jpg")
 
     
-    time.sleep(10)
+
     if correct != "True" and correct != "False":
         original_image = original_image.save(f"./pictures/None_{predicted_class}_{timestr}.jpg")
     
