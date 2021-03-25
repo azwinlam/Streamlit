582bae5638415d204d8fa3fc6160216b14aeaf58
azwinlam <azwin.lam@gmail.com>
2021-03-25 13:45:53 +0800

    /

--- beer/app.py
+++ beer/app.py
@@ -171,7 +171,7 @@ if uploaded_file is not None:
         
     correct = "None"
     timestr = time.strftime("%Y%m%d-%H%M%S")
-    col2.header("Was Your Beer Classified Correctly?")
+    col2.header("Is this a {predicted_class?")
     if col2.button("Yes"):
         col2.text("Thank you!")
         correct = "True"
