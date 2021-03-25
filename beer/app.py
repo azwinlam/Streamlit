dd8a16c77196d6f03b4d964a1cc5d9579e76492d
azwinlam <azwin.lam@gmail.com>
2021-03-25 15:35:24 +0800

    .

--- beer/app.py
+++ beer/app.py
@@ -44,7 +44,7 @@ if uploaded_file is None:
         sample = True
 
 ## Model Loading
-model = tf.keras.models.load_model('test.h5')
+model = tf.keras.models.load_model('MAR25AFTERNOON.h5')
 class_names = ['Asahi', 'Blue Girl', 'Blue Ice', 'Budweiser', 'Carlsberg', 'Corona', 'Guinness', 'Heineken', 'Kingway', 'Kirin', 'SOL', 'San Miguel', 'San Miguel Light', 'Stella', 'Tiger', 'Tsingtao']
 
 
