058514f6382e29f9e45b89b4b54ddfa1fb3aecaa
azwinlam <azwin.lam@gmail.com>
2021-03-26 17:08:13 +0800

    /

--- beer/app.py
+++ beer/app.py
@@ -112,34 +112,7 @@ class_names = ['Asahi', 'Blue Girl', 'Blue Ice', 'Budweiser', 'Carlsberg', 'Coro
 if uploaded_file is not None:
     if sample == True:
         try:
-          original_image = Image.open(image_path)
-          fixed_image = ImageOps.exif_transpose(original_image)
-          image_to_resize = img_to_array(fixed_image)
-        
-          resized = tf.image.resize(image_to_resize, [224, 168], method="bilinear",antialias=False)
-          img_array = tf.keras.preprocessing.image.img_to_array(resized)
-        
-          img_array = tf.expand_dims(img_array, 0) # Create a batch
-        
-          predictions = model.predict(img_array)
-          score = tf.nn.softmax(predictions[0])
-        
-          img_show = tf.squeeze(
-              img_array , axis=None, name=None)
-          
-          predicted_class = class_names[np.argmax(score)]
-        
-          st.write(f"This image most likely belongs to {predicted_class}")
-        
-          percentages = [i * 100 for i in predictions.tolist()[0]]
-        
-          results = zip(class_names, percentages)
-          sorted_by_second = sorted(results, key=lambda tup: tup[1],reverse=True)
-        
-        
-          for i in sorted_by_second[:2]:
-            st.write(i)
-        
+          predicted_class = load_model()
         except:
           pass
     else:
